c23f347a62d6063538bd66d0aa20b45e5d8d8ad9
anupamryadav <anupamryadav@gmail.com>
2015-10-11 09:40:33 +0530

    index

--- controller/indexController.js
+++ controller/indexController.js
@@ -11,7 +11,50 @@ app.controller('customersCtrl', function($scope, $http) {
 
   $http.get("data/Mobile.json")
   .success(function (response) {
-    $scope.product1 = response.products;
+    $scope.Mobile = response.products;
+  });  
+
+
+$http.get("data/Boy.json")
+  .success(function (response) {
+    $scope.Boy = response.products;
+  });  
+
+
+  $http.get("data/Girls.json")
+  .success(function (response) {
+    $scope.Girls = response.products;
+  });  
+
+  $http.get("data/computer.json")
+  .success(function (response) {
+    $scope.computer = response.products;
+  });  
+
+
+  $http.get("data/Kitchenware.json")
+  .success(function (response) {
+    $scope.Kitchenware = response.products;
+  });  
+
+  $http.get("data/KidFootwear.json")
+  .success(function (response) {
+    $scope.KidFootwear = response.products;
+  });  
+
+  $http.get("data/Watches.json")
+  .success(function (response) {
+    $scope.Watches = response.products;
+  });  
+
+  $http.get("data/Sunglasses.json")
+  .success(function (response) {
+    $scope.Sunglasses = response.products;
+  });  
+  
+ $http.get("data/Beauty.json")
+  .success(function (response) {
+    $scope.Beauty = response.products;
   });  
 
   }
