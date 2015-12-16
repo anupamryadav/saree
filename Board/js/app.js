5865f7c253cd3cefc91a9f51d987d7d088134953
anupamryadav@gmail.com <@123ananya>
2015-12-16 20:41:28 +0530

    file

--- Board/js/app.js
+++ Board/js/app.js
@@ -1,35 +1,40 @@
 angular.module('infiniteScrollTutorial', ['infinite-scroll'])
         .controller('infiniteScrollController', function ($scope, $http) {
 $scope.totalDisplayed = 6
-            
+            scope.users=[];
+  $scope.productBaseInfo=[];
+ 
+
 $scope.showData = function( ){
 
-              $http.get("data/saree.json")
-  .success(function (response) 
-           {
-           	$scope.companies=[];
-                  $scope.companies = response.productInfoList;
+        $http.get("data/Footware.json")
+  .success(function (response) {
 
-                   angular.forEach($scope.companies, function(items)
-                   {
-                   	angular.forEach(items, function(item)
-                   	{
-                   		console.log("dsfdsfsd");
-                   		if (item.productAttributes != null) 
-                   		{
-                   		$scope.users.push(item);
-                   		 };
-                   		
-                   	})
-                     
-               })
+   //scope.users = response.productInfoList;
 
-             
-                  
-              }); 
+    if (response.productInfoList!=null) {
+      for (var i = response.productInfoList.length - 1; i >= 0; i--) 
+      {
+       $scope.productBaseInfo.push(response.productInfoList[i].productBaseInfo);
+     };
+    };
+      
+       if ($scope.productBaseInfo!=null) {
+      for (var i = $scope.productBaseInfo.length - 1; i >= 0; i--) 
+      {
+       $scope.users.push($scope.productBaseInfo[i].productAttributes);
+     };
+    };
+    
+      
+
+  });   
 }
 
            
+
+          
+
 
                       
 $scope.getMoreData = function () {
